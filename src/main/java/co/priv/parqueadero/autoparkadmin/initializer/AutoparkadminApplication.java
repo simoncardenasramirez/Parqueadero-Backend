6dede205cc6d2cc34e193fbb6f990a425dc0345a
simon cardenas~ <simoncardenasramirez41@gmail.com>
2024-05-12 17:33:32 -0500

    carga inicial

---
+++ src/main/java/co/priv/parqueadero/autoparkadmin/initializer/AutoparkadminApplication.java
@@ -0,0 +1,13 @@
+package co.priv.parqueadero.autoparkadmin.initializer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoparkadminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoparkadminApplication.class, args);
+	}
+
+}
